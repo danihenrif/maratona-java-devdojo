65ccd8de4b8f6d425c65a7b0ce7b861f6cdbde53
danihenrif <danielhrqs4@yahoo.com>
2024-08-22 17:38:53 -0300

    aula10

---
+++ src/academy/devdojo/maratonajava/introducao/Aula10TiposPrimitivos.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula10TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+    }
+}
