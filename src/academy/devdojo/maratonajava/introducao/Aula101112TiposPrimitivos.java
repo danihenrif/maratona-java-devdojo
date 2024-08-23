a9772ec1e4247599c99aa25a7a3bd8aaa42f4c14
danihenrif <danielhrqs4@yahoo.com>
2024-08-23 00:19:41 -0300

    aula11 e 12

---
+++ src/academy/devdojo/maratonajava/introducao/Aula101112TiposPrimitivos.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula101112TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        float x = 2500.0F;
+        float y = (float) 2500.0D;
+        //Cast
+        int age = (int) 10000000000L;
+        System.out.println(age);
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
--- src/academy/devdojo/maratonajava/introducao/Aula10TiposPrimitivos.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.maratonajava.introducao;
-
-public class Aula10TiposPrimitivos {
-    public static void main(String[] args) {
-        // int, double, float, char, byte, short, long, boolean
-    }
-}
