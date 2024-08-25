8d7695400701d9d4eae31149ee55a1ee39e65d13
danihenrif <danielhrqs4@yahoo.com>
2024-08-24 23:00:36 -0300

    Aula 64 - Arrays com objetos

---
+++ src/academy/devdojo/maratonajava/javacore/DAssociacao/test/JogadorTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.DAssociacao.test;
+
+import academy.devdojo.maratonajava.javacore.DAssociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Messi");
+        Jogador jogador3 = new Jogador("C.Ronaldo");
+
+        Jogador[] jogadores = new Jogador[]{jogador1, jogador2, jogador3};
+
+        for(Jogador jogador : jogadores){
+            jogador.imprime();
+        }
+    }
+
+}
