73601a92b0cd4250a23947d71c5724ef0048ded5
danihenrif <danielhrqs4@yahoo.com>
2024-08-26 17:45:49 -0300

    Aula 76 - Sobrescrita do to string ()

---
+++ src/academy/devdojo/maratonajava/javacore/FToString/test/AnimeTest03.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.FToString.test;
+
+import academy.devdojo.maratonajava.javacore.FToString.dominio.Anime;
+
+public class AnimeTest03 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr.Strone");
+        System.out.println(anime.toString());
+    }
+}
